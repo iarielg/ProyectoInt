e04a355f9b5dd386d7d0da1743ced4662430ff97
Mauricio Suárez <mauricio_sq@comunidad.unam.mx>
2021-11-05 12:19:06 -0600

    Carrusel | creación de carrusel al index

---
+++ src/js/nosotros.js
@@ -0,0 +1,80 @@
+class person{
+    
+    name = "";
+    img = "";
+    work = "";
+    email = "";
+    resume = "";
+    linkedin = "";
+    github = "";
+
+
+    constructor (name, img, work, email, resume, linkedin, github){
+        
+        this.name = name.toUpperCase();
+        this.img = `${img}`;
+        this.work = `${work}`;
+        this.email = email.toLowerCase();
+        this.resume = `"${resume}"`;
+        this.linkedin = `${linkedin}`;
+        this.github = `${github}`;
+        
+        
+    }//constructor
+
+    printInfo(div){
+        div.innerHTML +=    `<div class="card-section border rounded p-3">
+            <div class="card-header-first rounded pb-5">
+                <h4 class="card-header-title text-white pt-1">${this.work}</h4>
+            </div>
+            <div>
+                <center>
+                    <img class="circular--portrait" src="${this.img}"
+                        class="img-fluid rounded-start" alt="perfil">
+                </center>
+            </div>
+            <div class="card-body text-center">
+                <h4 class="card-text text-white">${this.name}</h4>
+                <p class="card-text text-white-50">${this.resume}</p>
+            </div>
+            <div>
+                <a href="${this.github}"><i class="bi bi-github iconos card-section"></i></a>
+                <a href="${this.linkedin}"><i class="bi bi-linkedin iconos card-section"></i></a>
+                <a href="https://mail.google.com/mail/u/0/?view=cm&amp;fs=1&amp;to=${this.email}&amp;tf=1"><i class="bi bi-envelope-fill iconos card-section"></i></a>
+            </div>
+        </div>
+    </div>` 
+    }//printInfo
+}//class persons
+
+let divP0 = document.getElementById("container-1");
+let divP1 = document.getElementById("container-2");
+let divP2 = document.getElementById("container-3");
+let divP3 = document.getElementById("container-4");
+let divP4 = document.getElementById("container-5");
+let divP5 = document.getElementById("container-6");
+let divP6 = document.getElementById("container-7");
+// Se declaran los divs donde se coloca cada Card
+
+/*Resumen de Rafa, era mucho texto y lo definí en dos variables*/
+let resumeRafa1 = "Desarrollador de software con experiencia en equipos de trabajo con metodología ágiles, encargados del desarrollo de aplicaciones web escalables,";
+let resumeRafa2 = "robustas y seguras implementado las tecnologías con Java-EE, Maven y Spring Boot del lado del back-end y HTML5, CSS, JavaScript y Bootstrap en el apartdado front-end.";
+
+
+let ariel = new person ("J. Ariel Gonzalez", "/mnt/Date/pruebaNavBar/src/img/ariel2.jpg", "Java FullStack Developer", "arielgonzalezdelacruz@gmail.com", "Enamorado de la programacion", "https://www.linkedin.com/in/jarielgc/", "https://github.com/iarielg");
+let mau = new person ("Mauricio Suárez Quintero", "./../src/Mauricio.jpg", "Java FullStack Developer", "mauricio_sq@comunidad.unam.mx", "I'm a Java Full Stack developer and mathematician.", "https://www.linkedin.com/in/mauriciosuarezq", "https://github.com/MauricioSQ");
+let Ahmad = new person ("Ahmad Dowidar", "./../src/Ahmad.jpg", "Java FullStack Developer", "a7mad.dowidar@gmail.com","If you try to fail, and succeed, which have you done?", "https://www.linkedin.com/in/ahmad-dowidar/", "https://github.com/theDowidar");
+let david = new person ("David Daniel Hernández", "./../src/David.jpg", "Java FullStack Developer", "david.danielhdz@gmail.com", "Me gusta la tecnología y la automatización.", "https://www.linkedin.com/in/dhdavid", "https://github.com/david-danie");
+let rafa = new person ("Rafael Osnaya de Lucio", "./../src/Rafael.png" ,"Java FullStack Developer", "osnayadeluciorafael@hotmail.com", resumeRafa1 + resumeRafa2, "https://www.linkedin.com/in/rafael-osnaya-de-lucio-17b01720b/", "https://github.com/RafaOsnaya");
+let yaqui = new person ("Yaki Zeltzin Romero Trejo", "./../src/Yaki.jfif" ,"Java FullStack Developer", "zeltzin.rom96@gmail.com", "Disfruto mucho crear productos a través del código", "https://www.linkedin.com/in/yaki-zeltzin-romero-trejo-7a7471182/", "https://github.com/Yaki-Zeltzin");
+let arely = new person ("Areli Garcia Reyes", "./../src/ariel.jpg" ,"Java FullStack Developer", "arielgonzalezdelacruz@gmail.com", "Lorem ipsum", "https://www.linkedin.com/in/jarielgc/", "https://github.com/iarielg");
+// Se crean los nuevos objetos persona
+
+mau.printInfo(divP0);
+ariel.printInfo(divP1);
+Ahmad.printInfo(divP2);
+david.printInfo(divP3);
+rafa.printInfo(divP4);
+yaqui.printInfo(divP5);
+arely.printInfo(divP6);
+// Se imprime la info
