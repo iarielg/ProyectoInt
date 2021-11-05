521539140241d282f095aa0cb795fc7a0310ccba
zeltzin <zeltzin.rom96@gmail.com>
2021-11-05 12:14:13 -0600

    Importación de la barra de navegación en todas las páginas html

---
+++ src/js/navbar.js
@@ -0,0 +1,57 @@
+let navbar_root = document.getElementById('navbar_root');
+
+export function nav (div) {
+
+div.innerHTML = `
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="nav">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="./../index.html"><img src="./../src/img/logonev.png" alt="logoNeverDiesCo"
+            width="60" height="60" class="d-inline-block align-text-top"><!-- Navbar --></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="./../pages/galeria.html">GALERÍA</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                COMUNIDAD
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="./../pages/noticiasEventos.html">NOTICIAS/EVENTOS</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="./../pages/sponsor.html">SPONSOR</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="./../pages/servicios.html">SERVICIOS</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="./../pages/sucursales.html">SUCURSALES</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="./../pages/contacto.html">CONTACTO</a>
+            </li>
+
+            <li class="nav-item"><!-- Icono Carrito Compras -->
+                <a class="nav-link" href="./../pages/carritoCompras.html"><i class="bi bi-cart2"></i></a>
+            </li>
+            </ul>
+            <form class="d-flex">
+            <a class="nav-link" href="./../pages/login.html">LOGIN</a>
+            </form>
+        </div>
+    </div>
+</nav>`
+}
+nav(navbar_root)
+
