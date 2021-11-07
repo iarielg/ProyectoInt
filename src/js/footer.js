6501ba5082de1146671d2992224d6df5e4444c53
theDowidar <a7mad.dowidar@gmail.com>
2021-11-07 09:47:31 -0600

    fix footer responsive

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,58 +1,48 @@
 class theFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = 
-            `<footer id="footer" class="bg-dark text-white">
-                <div class="container-fluid">
+           `<footer id="footer" class="text-white bg-dark">
+                <div class="container-fluid pt-5 pb-5">
                     <div class="row">
-
                         <!--Never Dies logo-->
-                        <div class="col-2 align-self-center text-center">
-                            <a class=""><img src="../src/img/logonev.png" alt="Never Dies Logo" class="" width="120" height="120"></a>
+                        <div class="col-md-2 align-items-center text-center">
+                            <img src="../src/img/logonev.png" alt="" width="120" height="120">
                         </div>
-                            
+
                         <!--contacts-->
-                        <div class="col-7 align-self-center">
-                            <ul class="">
-                                <li class="list-unstyled m-4">
-                                    <span class="rounded footer-icons">
-                                        <i class="bi bi-instagram"></i>
-                                    </span>
-                                    <a href="">/neverdiesco</a>
+                        <div class="col-md-8 align-items-center text-center">
+                            <ul class="list-unstyled p-10">
+                                <li class="">
+                                    <i class="bi bi-instagram text-white"></i>
+                                    <a href="https://www.instagram.com/neverdiesoficial">/neverdiesco</a>
                                 </li>
-                                <li class="list-unstyled m-4">
-                                    <span class="">
-                                        <i class="bi bi-facebook"></i>
-                                    </span>
-                                    <a href="">/NeverDiesCo</a>
+                                <li class="">
+                                    <i class="bi bi-facebook text-white"></i>
+                                    <a href="https://www.facebook.com/NeverDiesCo/">NeverDiesCo</a>
                                 </li>
-                                <li class="list-unstyled m-4">
-                                    <span>
-                                        <i class="bi bi-whatsapp"></i>
-                                    </span>
-                                    +52 1 56 1264 9334
+                                <li class="">
+                                    <i class="bi bi-whatsapp text-white"></i>
+                                    <a href="https://wa.me/+5215612649334">+52 1 56 1264 9334</a>
                                 </li>
-                                <li class="list-unstyled m-4">
-                                    <span>
-                                        <i class="bi bi-envelope"></i>
-                                    </span>
+                                <li class="">
+                                    <i class="bi bi-envelope text-white"></i>
                                     <a href="mailto:contacto@neverdiesco.com">contacto@neverdiesco.com</a>
                                 </li>
                             </ul>
                         </div>
 
                         <!--In Code logo-->
-                        <div class="col-3 align-self-center text-center">
-                            <p class"text-center">Sitio creado por:</p>
-                            <a href="./../pages/nosotros.html"><img class="" src="../src/img/logoInCodeWeb.png" alt="In Code logo" width="180" height="80"></a>
+                        <div class="col-md-2 align-items-center text-center">
+                            <p class="align-items-center text-center">Sitio creado por:</p>
+                            <a href="" class="navbar-brand"><img src="../src/img/logoInCodeWeb.png" alt="" width="180" height="70"></a>
                         </div>
-
                     </div>
                 </div>
-
-                    <!--copyright-->
-                    <div class="row">
-                        <p class="d-flex justify-content-center">Copyright &copy; NeverDiesCo. 2021</p>
-                    </div>
+                <div class="container">
+                <!--copyright-->
+                <div class="row mr-0">
+                    <p class="text-center">Copyright &copy; NeverDiesCo. 2021</p>
+                </div>
                 </div>
             </footer>`
     }
