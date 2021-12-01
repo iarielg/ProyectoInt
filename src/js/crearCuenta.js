07ccd23e1928aed99db3ec960a51b8472985df32
Mauricio Suárez <mauricio_sq@comunidad.unam.mx>
2021-11-30 22:51:13 -0600

    Validación | valida en crear usuario en el localstorage

--- src/js/crearCuenta.js
+++ src/js/crearCuenta.js
@@ -1,4 +1,3 @@
-
 //funcion validar formulario
 function validacionDeFormulario(e) {
     e.preventDefault();
@@ -31,52 +30,51 @@ function validacionDeFormulario(e) {
     for (let i = 0; i <= fieldPhone.length; i++) {
         if ((fieldPhone.charAt(i) >= '0') && (fieldPhone.charAt(i) <= '9')) {
             phoneCont += 1;
-        }
-    }
+        }//if
+    }//for
     for (let i = 0; i <= fieldName.length; i++) {
         if (((fieldName.charAt(i) >= 'A') && (fieldName.charAt(i) <= 'Z')) || ((fieldName.charAt(i) >= 'a') && (fieldName.charAt(i) <= 'z')) || (fieldName.charAt(i) === ' ')) {
             nameCont += 1;
-        }
-    }
+        }//if
+    }//for
     for (let i = 0; i <= fieldPass0.length; i++) {
         if (((fieldPass0.charAt(i) >= 'A') && (fieldPass0.charAt(i) <= 'Z')) || ((fieldPass0.charAt(i) >= 'a') && (fieldPass0.charAt(i) <= 'z')) || ((fieldPass0.charAt(i) >= '0') && (fieldPass0.charAt(i) <= '9'))) {
             passCont += 1;
-        }
-    }
+        }//if
+    }//for
 
     if ((fieldName.length >= 2) && (fieldName.length === nameCont)) {
         addUserCont += 1;
         //console.log(`Nombre válido: ${fieldName}`);
         document.querySelector('.name').classList.remove('is-invalid');
         document.querySelector('.name').classList.add('is-valid');
-    } else {
-        //console.log(`Nombre invalido: ${fieldName}`);
-        document.querySelector('.name').classList.remove('is-valid');
-        document.querySelector('.name').classList.add('is-invalid');
-    } // ====================== validación nombre
+        } else {
+            //console.log(`Nombre invalido: ${fieldName}`);
+            document.querySelector('.name').classList.remove('is-valid');
+            document.querySelector('.name').classList.add('is-invalid');
+    } // === validación nombre
 
     if ((fieldEmail.length > 8) && (fieldEmail.indexOf('@') >= 0) && ((fieldEmail.indexOf(".com") >= 0) || (fieldEmail.indexOf(".edu") >= 0))) {
         addUserCont += 1;
         //console.log(`Correo válido: ${fieldEmail}`);
         document.querySelector('.email').classList.remove('is-invalid');
         document.querySelector('.email').classList.add('is-valid');
-    } else {
+        } else {
         //console.log(`Correo invalido: ${fieldEmail}`); 
         document.querySelector('.email').classList.remove('is-valid');
         document.querySelector('.email').classList.add('is-invalid');
-    } // ====================== validación email
+    } // === validación email
 
     if ((fieldPhone.length == 10) && (fieldPhone.length === phoneCont)) {
         addUserCont += 1;
         //console.log(`Teléfono válido: ${fieldPhone}`);
         document.querySelector('.phone').classList.remove('is-invalid');
         document.querySelector('.phone').classList.add('is-valid');
-    } else {
+        } else {
         //console.log(`Teléfono invalido: ${fieldPhone}`);
         document.querySelector('.phone').classList.remove('is-valid');
         document.querySelector('.phone').classList.add('is-invalid');
-
-    } // ====================== validación teéfono
+    } // === validación teéfono
 
     if ((fieldPass0.length >= 10) && (fieldPass0.length === passCont)) {
         addUserCont += 1;
@@ -87,24 +85,22 @@ function validacionDeFormulario(e) {
         //console.log(`Contraseña invalida: ${fieldPass0}`);
         document.querySelector('.pass0').classList.remove('is-valid');
         document.querySelector('.pass0').classList.add('is-invalid');
-    } // ====================== validación contraseña
+    } // === validación contraseña
 
     if (fieldPass0 === fieldPass1) {
         addUserCont += 1;
         //console.log(`Contraseña válida: ${fieldPass1}`);
         document.querySelector('.pass1').classList.remove('is-invalid');
         document.querySelector('.pass1').classList.add('is-valid');
-    } else {
+        } else {
         //console.log(`Contraseña invalida: ${fieldPass1}`);
         document.querySelector('.pass1').classList.remove('is-valid');
         document.querySelector('.pass1').classList.add('is-invalid');
-    } // ====================== validación contraseña
+    } // === validación contraseña
 
     // let usuario = [];
 
-
-    if (addUserCont == 5) {
-        //se crea objeto usuario
+    if (addUserCont == 5) { //se crea objeto usuario
         let newUser = {
             "name": `${fieldName}`,
             "email": `${fieldEmail}`,
@@ -112,16 +108,16 @@ function validacionDeFormulario(e) {
             "pass0": `${fieldPass0}`,
         }
         //se trae de localStorage el objeto creado
-        let usuarios = window.localStorage.getItem('usuarios');
-        // if (storeList == null) {
+        let usuarios = window.localStorage.getItem('usuarios'); //se traé todo lo del localStorage
             let storeList;
             //si usuarios esta vacío crear el objeto del administrador
             if (!usuarios ) {
             storeList = [
-                {"nombre":"yaki", "email":"zeltzin@gmail.com", "pass0":"yakiromerotrejo"}
+                {"nombre":"yaki", "email":"zeltzin@gmail.com", "pass0":"yakiromerotrejo"} //Default
             ];
+            //stringify convierte un objeto o valor de JavaScript en una cadena de texto JSON
             //transformar a string el objeto del admin
-            localStorage.setItem('usuarios', JSON.stringify(storeList))
+            localStorage.setItem('usuarios', JSON.stringify(storeList)) //Aquí añadimos al localStorage
         } else {
             //si existen objetos usuario convertirlos a JSON
             storeList = JSON.parse(usuarios);
@@ -131,7 +127,7 @@ function validacionDeFormulario(e) {
         usuarioExistente(storeList, newUser)
         
     }
-}
+}//function validacionDeFormulario
 //se crea la funcion que valida si el usuario ya existe o no 
 function usuarioExistente(arregloUsuario, objUsuario) {
    console.log(arregloUsuario) 
@@ -140,30 +136,36 @@ function usuarioExistente(arregloUsuario, objUsuario) {
         console.log(usuario)
         console.log(objUsuario)
         //si son iguales retorna el objeto nuevo
-        if (usuario.email == objUsuario.email && usuario.pass0 ==  objUsuario.pass0){
+        if (usuario.email == objUsuario.email || usuario.phone ==  objUsuario.phone){
             return objUsuario
-            // console.log('el usuario ya existe');
+            console.log('el usuario ya existe');
         }
     })
 console.log(newArreglo)
 //si nuevo arreglo es diferente agregar el objUsuario
     if(!newArreglo ){
         arregloUsuario.push(objUsuario)
-    
+
         // convierte arreglousuario  a string
         localStorage.setItem('usuarios', JSON.stringify(arregloUsuario))
-    
+        window.location.href="./../pages/login.html"
     }else{
-        
-        alert('usuario ya existe')
-
-      
-
+        // alert('El usuario ya existe')
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: '¡Esta cuenta ya existe!',
+            // footer: '<a href="./../pages/login.html">¿Intenta iniciar sesión?</a>'
+            confirmButtonColor: "black",
+            width: '40%',
+            Height: '40%'
+          })
+          
     }
-    
-};
-    const form1 = document.getElementById('form_nc');
-    form1.addEventListener('submit', validacionDeFormulario);
-  console.log(usuarios); 
-  console.log(storeList);
+}; //function usuarioExistente
+
+const form1 = document.getElementById('form_nc');
+form1.addEventListener('submit', validacionDeFormulario);
+console.log(usuarios); 
+console.log(storeList);
 
